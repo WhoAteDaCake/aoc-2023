4c5973ab24029905a00ee3d78f5558c80d903bd8
WhoAteDaCake <doitsimplefy@gmail.com>
2023-12-04 16:58:49 +0000

    feat: split solutions

--- Problem4/Solution.fs
+++ Problem4/Solution.fs
@@ -11,16 +11,20 @@ let intersect (xs:'a seq) (ys: 'a seq) = xs.Intersect(ys)
 let parseNums (str: string) =
     Regex.Matches(str, "\d+") |> Seq.map (fun c -> Int32.Parse c.Value)
     
-let solve1 (row: string) =
+let solveRow (row: string) =
     let [winning;mine] = (row.Split(": ")[1]).Split(" | ") |> List.ofSeq in
     let winning, mine = parseNums winning, parseNums mine in
     let matching = intersect winning mine |> Seq.length in
     let matching = matching - 1 in
     Math.Pow((double)2, matching)
     
-let run (ls:string list): string =
-    let result = List.fold (fun sum row -> sum + (int)(solve1 row)) 0 ls in
+let solve1 (ls:string list): string =
+    let result = List.fold (fun sum row -> sum + (int)(solveRow row)) 0 ls in
     result.ToString()
+    
+let solve2 (ls: string list): string
+    let result = List.map solveRow ls in
+    ""
 
-let spec = ("./Problem4/input_large.txt", run)
+let spec = ("./Problem4/input_large.txt", solve2)
 
