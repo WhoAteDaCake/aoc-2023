10299e50f860a42547e484b3e3c59f4a9d1bd67e
WhoAteDaCake <doitsimplefy@gmail.com>
2023-12-04 20:33:33 +0000

    feat: solution

--- Problem4/Solution.fs
+++ Problem4/Solution.fs
@@ -4,27 +4,43 @@ open System
 open System.Text.RegularExpressions
 open System.Linq
 
-// let nRe =/
-
 let intersect (xs:'a seq) (ys: 'a seq) = xs.Intersect(ys)
 
 let parseNums (str: string) =
     Regex.Matches(str, "\d+") |> Seq.map (fun c -> Int32.Parse c.Value)
-    
-let solveRow (row: string) =
+
+let getMatching (row: string) =    
     let [winning;mine] = (row.Split(": ")[1]).Split(" | ") |> List.ofSeq in
     let winning, mine = parseNums winning, parseNums mine in
-    let matching = intersect winning mine |> Seq.length in
-    let matching = matching - 1 in
-    Math.Pow((double)2, matching)
+    intersect winning mine |> Seq.length
+
+let solveRow (row: string) =
+    let matching = (getMatching row) - 1 in
+    if matching < 0 then 0.0 else Math.Pow((double)2, matching)
     
 let solve1 (ls:string list): string =
     let result = List.fold (fun sum row -> sum + (int)(solveRow row)) 0 ls in
     result.ToString()
     
-let solve2 (ls: string list): string
-    let result = List.map solveRow ls in
-    ""
+let fillScores (ls: int list) =
+    let max = List.length ls in
+    let limit = max - 1 in 
+    let arr = Array.create max 1 in
+    let rec loop idx acc =
+        match acc with
+        | [] -> arr
+        | x::xs ->
+           for i in idx .. Math.Min(idx + x - 1, limit) do
+                arr[i] <- arr[i] + arr[idx - 1]
+           loop (idx + 1) xs
+    in
+    loop 1 ls
+
+let solve2 (ls: string list): string =
+    let result = List.map getMatching ls in
+    let items = fillScores result in
+    let all = Array.fold (fun total n -> total + n) 0 items in
+    all.ToString()
 
 let spec = ("./Problem4/input_large.txt", solve2)
 
